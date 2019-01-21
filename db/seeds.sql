30c85f6519049d348f5d1825dc15981bfd24cd18
Mike Worthington <mworthington888@gmail.com>
2019-01-21 12:32:38 -0600

    schema and seeds files created.

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name)
+VALUES('BabyMetal-Japanese');
+
+INSERT INTO burgers(burger_name)
+VALUES('Keegan Michael Key-Lime Sliders');
+
+INSERT INTO burgers(burger_name)
+VALUES('Tofu-Yuck-Alicious');
+
