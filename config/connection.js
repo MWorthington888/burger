74c0b82f8caccfa99d0ae6ca4c4c0f26ee746823
Mike Worthington <mworthington888@gmail.com>
2019-01-21 13:23:58 -0600

    Added code to orm.js and connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,24 @@
 
 // Modules that need to be installed prior to operation -----------------------------------
 
-const mysql = require("mysql");// Connection verbage ------------------------------------------------------------------------
 
-
+tion.js 
+// Global
+const mysql = require("mysql");
+// Connection
 const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "Michael Worthington",
-    password: "gracieroot",
-    database: "burgers_db"
+	host: "localhost",
+	user: "Michael Worthington",
+	password: "gracieroot",
+	database: "burger_db"
 });
+​
+connection.connect(function(err) {
+	if (err) {
+		console.error("error connecting: " + err.stack);
+		return;
+	}
+	console.log("connected as id " + connection.threadId);
+});
+​
+module.exports = connection;
